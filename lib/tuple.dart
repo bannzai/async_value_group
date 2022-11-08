0d9bc094b2f21b9e873639789477aa97c1c7a229
bannzai <kingkong999yhirose@gmail.com>
2022-11-08 22:30:55 +0900

    Add tuple file

---
+++ lib/tuple.dart
@@ -0,0 +1,421 @@
+class Tuple2<T1, T2> {
+  final T1 t1;
+  final T2 t2;
+
+  Tuple2(this.t1, this.t2);
+}
+
+class Tuple3<T1, T2, T3> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+
+  Tuple3(this.t1, this.t2, this.t3);
+}
+
+class Tuple4<T1, T2, T3, T4> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+
+  Tuple4(this.t1, this.t2, this.t3, this.t4);
+}
+
+class Tuple5<T1, T2, T3, T4, T5> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+
+  Tuple5(this.t1, this.t2, this.t3, this.t4, this.t5);
+}
+
+class Tuple6<T1, T2, T3, T4, T5, T6> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+
+  Tuple6(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6);
+}
+
+class Tuple7<T1, T2, T3, T4, T5, T6, T7> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+
+  Tuple7(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7);
+}
+
+class Tuple8<T1, T2, T3, T4, T5, T6, T7, T8> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+
+  Tuple8(
+      this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7, this.t8);
+}
+
+class Tuple9<T1, T2, T3, T4, T5, T6, T7, T8, T9> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+
+  Tuple9(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7, this.t8,
+      this.t9);
+}
+
+class Tuple10<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+
+  Tuple10(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7,
+      this.t8, this.t9, this.t10);
+}
+
+class Tuple11<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+
+  Tuple11(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7,
+      this.t8, this.t9, this.t10, this.t11);
+}
+
+class Tuple12<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+
+  Tuple12(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7,
+      this.t8, this.t9, this.t10, this.t11, this.t12);
+}
+
+class Tuple13<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+
+  Tuple13(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7,
+      this.t8, this.t9, this.t10, this.t11, this.t12, this.t13);
+}
+
+class Tuple14<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+  final T14 t14;
+
+  Tuple14(this.t1, this.t2, this.t3, this.t4, this.t5, this.t6, this.t7,
+      this.t8, this.t9, this.t10, this.t11, this.t12, this.t13, this.t14);
+}
+
+class Tuple15<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14,
+    T15> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+  final T14 t14;
+  final T15 t15;
+
+  Tuple15(
+      this.t1,
+      this.t2,
+      this.t3,
+      this.t4,
+      this.t5,
+      this.t6,
+      this.t7,
+      this.t8,
+      this.t9,
+      this.t10,
+      this.t11,
+      this.t12,
+      this.t13,
+      this.t14,
+      this.t15);
+}
+
+class Tuple16<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+    T16> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+  final T14 t14;
+  final T15 t15;
+  final T16 t16;
+
+  Tuple16(
+      this.t1,
+      this.t2,
+      this.t3,
+      this.t4,
+      this.t5,
+      this.t6,
+      this.t7,
+      this.t8,
+      this.t9,
+      this.t10,
+      this.t11,
+      this.t12,
+      this.t13,
+      this.t14,
+      this.t15,
+      this.t16);
+}
+
+class Tuple17<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+    T16, T17> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+  final T14 t14;
+  final T15 t15;
+  final T16 t16;
+  final T17 t17;
+
+  Tuple17(
+      this.t1,
+      this.t2,
+      this.t3,
+      this.t4,
+      this.t5,
+      this.t6,
+      this.t7,
+      this.t8,
+      this.t9,
+      this.t10,
+      this.t11,
+      this.t12,
+      this.t13,
+      this.t14,
+      this.t15,
+      this.t16,
+      this.t17);
+}
+
+class Tuple18<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+    T16, T17, T18> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+  final T14 t14;
+  final T15 t15;
+  final T16 t16;
+  final T17 t17;
+  final T18 t18;
+
+  Tuple18(
+      this.t1,
+      this.t2,
+      this.t3,
+      this.t4,
+      this.t5,
+      this.t6,
+      this.t7,
+      this.t8,
+      this.t9,
+      this.t10,
+      this.t11,
+      this.t12,
+      this.t13,
+      this.t14,
+      this.t15,
+      this.t16,
+      this.t17,
+      this.t18);
+}
+
+class Tuple19<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+    T16, T17, T18, T19> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+  final T14 t14;
+  final T15 t15;
+  final T16 t16;
+  final T17 t17;
+  final T18 t18;
+  final T19 t19;
+
+  Tuple19(
+      this.t1,
+      this.t2,
+      this.t3,
+      this.t4,
+      this.t5,
+      this.t6,
+      this.t7,
+      this.t8,
+      this.t9,
+      this.t10,
+      this.t11,
+      this.t12,
+      this.t13,
+      this.t14,
+      this.t15,
+      this.t16,
+      this.t17,
+      this.t18,
+      this.t19);
+}
+
+class Tuple20<T1, T2, T3, T4, T5, T6, T7, T8, T9, T10, T11, T12, T13, T14, T15,
+    T16, T17, T18, T19, T20> {
+  final T1 t1;
+  final T2 t2;
+  final T3 t3;
+  final T4 t4;
+  final T5 t5;
+  final T6 t6;
+  final T7 t7;
+  final T8 t8;
+  final T9 t9;
+  final T10 t10;
+  final T11 t11;
+  final T12 t12;
+  final T13 t13;
+  final T14 t14;
+  final T15 t15;
+  final T16 t16;
+  final T17 t17;
+  final T18 t18;
+  final T19 t19;
+  final T20 t20;
+
+  Tuple20(
+      this.t1,
+      this.t2,
+      this.t3,
+      this.t4,
+      this.t5,
+      this.t6,
+      this.t7,
+      this.t8,
+      this.t9,
+      this.t10,
+      this.t11,
+      this.t12,
+      this.t13,
+      this.t14,
+      this.t15,
+      this.t16,
+      this.t17,
+      this.t18,
+      this.t19,
+      this.t20);
+}
