e0a65a805c7b75a0d04af95714b6b496739050d7
bannzai <kingkong999yhirose@gmail.com>
2022-11-09 12:30:35 +0900

    add export package

--- lib/async_value_group.dart
+++ lib/async_value_group.dart
@@ -1,7 +1,4 @@
 library async_value_group;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:async_value_group/tuple.dart';
+export 'package:riverpod/riverpod.dart';
