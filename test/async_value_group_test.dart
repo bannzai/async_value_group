4578366cde1653a8af57bd9990249d1f41b4f081
bannzai <kingkong999yhirose@gmail.com>
2022-11-09 12:39:28 +0900

    add first test case

--- test/async_value_group_test.dart
+++ test/async_value_group_test.dart
@@ -1,12 +1,19 @@
+import 'package:async_value_group/async_value_extension.dart';
 import 'package:flutter_test/flutter_test.dart';
 
 import 'package:async_value_group/async_value_group.dart';
 
 void main() {
-  test('adds one to input values', () {
-    final calculator = Calculator();
-    expect(calculator.addOne(2), 3);
-    expect(calculator.addOne(-7), -6);
-    expect(calculator.addOne(0), 1);
+  test('Simple AsyncValue case', () {
+    // Check nullish and non nullish values
+    const AsyncValue<int?> t1 = AsyncData(1);
+    const AsyncValue<String?> t2 = AsyncData("value");
+    const AsyncValue<double?> t3 = AsyncData(null);
+    const AsyncValue<String> t4 = AsyncData("value");
+    final group = AsyncValueGroup.group4(t1, t2, t3, t4);
+    expect(group.value!.t1, 1);
+    expect(group.value!.t2, "value");
+    expect(group.value!.t3, null);
+    expect(group.value!.t4, "value");
   });
 }
