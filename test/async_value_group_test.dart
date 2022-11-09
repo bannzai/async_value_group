61b978019da3077e74a08d563c5694dd4e52b535
bannzai <kingkong999yhirose@gmail.com>
2022-11-09 12:45:09 +0900

    add test case

--- test/async_value_group_test.dart
+++ test/async_value_group_test.dart
@@ -15,5 +15,33 @@ void main() {
     expect(group.value!.t2, "value");
     expect(group.value!.t3, null);
     expect(group.value!.t4, "value");
+    expect(group is AsyncData, true);
+  });
+
+  group("group2", () {
+    test("#AsyncValue", () {
+      const AsyncValue<int> t1 = AsyncData(1);
+      const AsyncValue<String> t2 = AsyncData("value");
+      final group = AsyncValueGroup.group2(t1, t2);
+      expect(group.value!.t1, 1);
+      expect(group.value!.t2, "value");
+      expect(group is AsyncData, true);
+    });
+    test("#AsyncLoading", () {
+      const AsyncValue<int> t1 = AsyncData(1);
+      const AsyncValue<String> t2 = AsyncLoading();
+      final group = AsyncValueGroup.group2(t1, t2);
+      expect(group.value, null);
+      expect(group.isLoading, true);
+      expect(group is AsyncLoading, true);
+    });
+    test("#AsyncError", () {
+      const AsyncValue<int> t1 = AsyncData(1);
+      const AsyncValue<String> t2 = AsyncError(FormatException(""), StackTrace.empty);
+      final group = AsyncValueGroup.group2(t1, t2);
+      expect(group.asData?.valueOrNull, null);
+      expect(group.error, isNotNull);
+      expect(group is AsyncError, true);
+    });
   });
 }
