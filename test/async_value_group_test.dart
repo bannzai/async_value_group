48924702c7dc2b1cb3c261d9a1a20996db046c00
bannzai <kingkong999yhirose@gmail.com>
2022-11-09 12:51:22 +0900

    add test case

--- test/async_value_group_test.dart
+++ test/async_value_group_test.dart
@@ -197,7 +197,7 @@ void main() {
       expect(group is AsyncError, true);
     });
   });
-  
+
   group("group7", () {
     test("#AsyncValue", () {
       const AsyncValue<int> t1 = AsyncData(1);
@@ -239,13 +239,13 @@ void main() {
       const AsyncValue<String> t5 = AsyncData("value");
       const AsyncValue<String> t6 = AsyncData("value");
       const AsyncValue<String> t7 = AsyncData("value");
-final group = AsyncValueGroup.group7(t1, t2, t3, t4, t5, t6, t7);
+      final group = AsyncValueGroup.group7(t1, t2, t3, t4, t5, t6, t7);
       expect(group.asData?.valueOrNull, null);
       expect(group.error, isNotNull);
       expect(group is AsyncError, true);
     });
   });
-  
+
   group("group8", () {
     test("#AsyncValue", () {
       const AsyncValue<int> t1 = AsyncData(1);
@@ -292,6 +292,62 @@ final group = AsyncValueGroup.group7(t1, t2, t3, t4, t5, t6, t7);
       const AsyncValue<String> t7 = AsyncData("value");
       const AsyncValue<String> t8 = AsyncData("value");
       final group = AsyncValueGroup.group8(t1, t2, t3, t4, t5, t6, t7, t8);
+      expect(group.asData?.valueOrNull, null);
+      expect(group.error, isNotNull);
+      expect(group is AsyncError, true);
+    });
+  });
+
+  group("group9", () {
+    test("#AsyncValue", () {
+      const AsyncValue<int> t1 = AsyncData(1);
+      const AsyncValue<String> t2 = AsyncData("value");
+      const AsyncValue<double> t3 = AsyncData(1.0);
+      const AsyncValue<String> t4 = AsyncData("value");
+      const AsyncValue<String> t5 = AsyncData("value");
+      const AsyncValue<String> t6 = AsyncData("value");
+      const AsyncValue<String> t7 = AsyncData("value");
+      const AsyncValue<String> t8 = AsyncData("value");
+      const AsyncValue<String> t9 = AsyncData("value");
+      final group = AsyncValueGroup.group9(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+      expect(group.value!.t1, 1);
+      expect(group.value!.t2, "value");
+      expect(group.value!.t3, 1.0);
+      expect(group.value!.t4, "value");
+      expect(group.value!.t5, "value");
+      expect(group.value!.t6, "value");
+      expect(group.value!.t7, "value");
+      expect(group.value!.t8, "value");
+      expect(group.value!.t9, "value");
+      expect(group is AsyncData, true);
+    });
+    test("#AsyncLoading", () {
+      const AsyncValue<int> t1 = AsyncData(1);
+      const AsyncValue<String> t2 = AsyncLoading();
+      const AsyncValue<double> t3 = AsyncData(1.0);
+      const AsyncValue<String> t4 = AsyncData("value");
+      const AsyncValue<String> t5 = AsyncData("value");
+      const AsyncValue<String> t6 = AsyncData("value");
+      const AsyncValue<String> t7 = AsyncData("value");
+      const AsyncValue<String> t8 = AsyncData("value");
+      const AsyncValue<String> t9 = AsyncData("value");
+      final group = AsyncValueGroup.group9(t1, t2, t3, t4, t5, t6, t7, t8, t9);
+      expect(group.value, null);
+      expect(group.isLoading, true);
+      expect(group is AsyncLoading, true);
+    });
+    test("#AsyncError", () {
+      const AsyncValue<int> t1 = AsyncData(1);
+      const AsyncValue<String> t2 =
+          AsyncError(FormatException(""), StackTrace.empty);
+      const AsyncValue<double> t3 = AsyncData(1.0);
+      const AsyncValue<String> t4 = AsyncData("value");
+      const AsyncValue<String> t5 = AsyncData("value");
+      const AsyncValue<String> t6 = AsyncData("value");
+      const AsyncValue<String> t7 = AsyncData("value");
+      const AsyncValue<String> t8 = AsyncData("value");
+      const AsyncValue<String> t9 = AsyncData("value");
+      final group = AsyncValueGroup.group9(t1, t2, t3, t4, t5, t6, t7, t8, t9);
       expect(group.asData?.valueOrNull, null);
       expect(group.error, isNotNull);
       expect(group is AsyncError, true);
